6c600c9f70df4fe5c003c6cb82e380f40f6b3ce9
yukako <tomtomko.0310@gmail.com>
2025-03-18 22:41:32 +0900

    3/18 大枠完成

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const mySwiper = new Swiper('.swiper', {
+   // Optional parameters
+   loop: true,
+
+   spaceBetween: 60,
+   centeredSlides: true,
+   slidesPerView: 2.5,
+
+
+   // Navigation arrows
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+
+
+
+ });
